dd165cff6435e3a48f954b3b12d0a93390e35b84
Benjamin J Hicks <73999671+HennyBicks01@users.noreply.github.com>
2024-09-04 04:49:03 -0400

    updated the menu screen to look shittier

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -6,10 +6,21 @@ signal join_game(code)
 @onready var host_button = $VBoxContainer/HostButton
 @onready var join_button = $VBoxContainer/JoinButton
 @onready var code_input = $VBoxContainer/CodeInput
+@onready var title_letters = $VBoxContainer/Title.get_children()
+
+var time = 0
+var wave_speed = 2
+var wave_height = 10
 
 func _ready():
 	host_button.connect("pressed", _on_host_button_pressed)
 	join_button.connect("pressed", _on_join_button_pressed)
+
+func _process(delta):
+	time += delta
+	for i in range(title_letters.size()):
+		var offset = sin(time * wave_speed + i * 0.5) * wave_height
+		title_letters[i].position.y = offset
 
 func _on_host_button_pressed():
 	emit_signal("host_game")
